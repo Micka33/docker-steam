d6a6e898281e83300a793a4b4a13bf8d1fa06731
Mickael L Cassy <mickael.cassy@gmail.com>
2014-08-13 18:02:08 +0200

    fixes some shit

--- docker_files/run_server.sh
+++ docker_files/run_server.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 cd /root/mounted
+# Fixes [S_API FAIL] SteamAPI_Init() failed; unable to locate a running instance of Steam, or a local steamclient.dll.
 export HOME="/root"
+file="/root/.steam/sdk32/steamclient.so"
+if ! [ -f "$file" ]
+then
+	mkdir -p ~/.steam/sdk32/
+	ln -s /etc/service/steamcmd/linux32/steamclient.so "$file"
+fi
+
+
 
 # Classic Casual
 # exec ./srcds_run -game csgo -console -usercon +game_type 0 +game_mode 0 +mapgroup mg_active
 # Classic Competitive
-exec ./srcds_run -game csgo -console -usercon +game_type 0 +game_mode 1 +mapgroup mg_bomb_se
+exec ./srcds_run -game csgo -console -usercon +game_type 0 +game_mode 1
 # Arms Race
 # exec ./srcds_run -game csgo -console -usercon +game_type 1 +game_mode 0 +mapgroup mg_armsrace
 # Demolition
